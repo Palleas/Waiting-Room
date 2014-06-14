b5f5691bc3a0e6a868d9b2382a29848d309d7d7a
Romain Pouclet <palleas@gmail.com>
2014-06-13 22:54:43 -0400

    Initial Commit

---
+++ Waiting-Room/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Waiting-Room
+//
+//  Created by Romain Pouclet on 2014-06-13.
+//  Copyright (c) 2014 Perfectly Cooked. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
