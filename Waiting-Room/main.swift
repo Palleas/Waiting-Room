e7f8da4be1a211bb550db7db55ed4f20fa3e749c
Romain Pouclet <palleas@gmail.com>
2014-06-14 00:03:02 -0400

    Quick and dirty POC
    * Fetching a random number fact every 2 seconds until the command is ran
    * Doesn't actually run the provided command for now because it's midnight
    and I've only been working on this because, well, a command was taking too
    much time.

--- Waiting-Room/main.swift
+++ Waiting-Room/main.swift
@@ -8,5 +8,44 @@
 
 import Foundation
 
-println("Hello, World!")
+func fetchFact() {
+    let session = NSURLSession.sharedSession()
+    let request = NSURLRequest(URL: NSURL.URLWithString("http://numbersapi.com/random/year"))
+    let fetchTask = session.dataTaskWithRequest(request, completionHandler: {data, response, error in
+        let content = NSString(data: data, encoding: NSUTF8StringEncoding)
+        println("FACT:\n\(content)\n")
+
+        // Waiting 2 seconds and fetching a new fact
+        let time = dispatch_time(DISPATCH_TIME_NOW, Int64(2 * NSEC_PER_SEC))
+        dispatch_after(time, dispatch_get_main_queue(), {
+            fetchFact()
+        })
+    })
+    fetchTask.resume()
+}
+
+let args = NSProcessInfo.processInfo()!.arguments
+
+// In case a command was not provided
+if args.count == 1 {
+    println("Please provide a command to run 😁")
+//    exit(1)
+}
+
+var over = false
+
+var task = NSTask()
+task.launchPath = "/bin/sleep"
+task.arguments = ["10"]
+task.terminationHandler = {task -> Void in
+    println("Task is over \(task.terminationStatus)")
+    exit(task.terminationStatus)
+}
+task.launch()
+
+fetchFact()
+
+while NSRunLoop.currentRunLoop().runMode(NSDefaultRunLoopMode, beforeDate: NSDate.distantFuture() as NSDate) {}
+
+println("done")
 
